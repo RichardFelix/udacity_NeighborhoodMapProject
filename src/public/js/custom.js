ce54bc61db396cd1a0bb2fff0f92cec99ab274c7
Richard Felix <rfelixmail@gmail.com>
2016-06-25 02:51:28 -0400

    comments: added the flickr api info to work on tommorow and finish up ajax calls for infoWindow

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -164,7 +164,7 @@ function initMap() {
     var marker = new google.maps.Marker({
       position: myLatLng,
       map: map,
-      title: 'Hello World!',
+      title: locations[x].name,
       animation: google.maps.Animation.DROP
     });
 
@@ -212,8 +212,8 @@ var list = document.getElementById('list'),
 // adding click function to the closeList div and
 // moving the list and menu button off to the side of the screen
 // and back again if off the screen already
-closeList.addEventListener("click", function(){
-  if(list.style.transform === "translateX(100%)") {
+closeList.addEventListener('click', function(){
+  if(list.style.transform === 'translateX(100%)') {
     list.style = 'transform:translateX(0%)';
     closeList.style = 'transform:translateX(0%)';
   }else{
@@ -221,3 +221,40 @@ closeList.addEventListener("click", function(){
     closeList.style = 'transform:translateX(1415%)';
   }
 });
+
+// Flikr
+// key
+// f0b0865d687bdae8485ab41a107f73e4
+//
+// Secret:
+// 1ac413079d365ce4
+
+// https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f0b0865d687bdae8485ab41a107f73e4&lat=40.7484444&lon=-73.9878441&format=json&content_type=1&per_page=10&page=1&radius=1&radius_units=mi
+//
+// Example
+//
+// var farmId, serverId, photoId, secret;
+// "https://farm " + farmId + ".staticflickr.com/" + serverId + "/" + photoId + "_" + secret + "_m.jpg";
+// https://farm1.staticflickr.com/2/1418878_1e92283336_m.jpg
+//
+// farm-id: 1
+// server-id: 2
+// photo-id: 1418878
+// secret: 1e92283336
+// size: m
+//
+// Size Suffixes
+//
+// The letter suffixes are as follows:
+// s	small square 75x75
+// q	large square 150x150
+// t	thumbnail, 100 on longest side
+// m	small, 240 on longest side
+// n	small, 320 on longest side
+// -	medium, 500 on longest side
+// z	medium 640, 640 on longest side
+// c	medium 800, 800 on longest side†
+// b	large, 1024 on longest side*
+// h	large 1600, 1600 on longest side†
+// k	large 2048, 2048 on longest side†
+// o	original image, either a jpg, gif or png, depending on source format
